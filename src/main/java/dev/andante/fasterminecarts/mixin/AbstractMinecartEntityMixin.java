4c64914d04ce4ec051ded9b129098b0090f97fe0
Daniel Orr <orrdaniel73@gmail.com>
2024-07-15 11:25:37 +0100

    Just players!

--- src/main/java/dev/andante/fasterminecarts/mixin/AbstractMinecartEntityMixin.java
+++ src/main/java/dev/andante/fasterminecarts/mixin/AbstractMinecartEntityMixin.java
@@ -1,5 +1,6 @@
 package dev.andante.fasterminecarts.mixin;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.entity.vehicle.MinecartEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +13,7 @@ public class AbstractMinecartEntityMixin {
     @Inject(method = "getMaxSpeed", at = @At("RETURN"), cancellable = true)
     private void onGetMaxSpeed(CallbackInfoReturnable<Double> cir) {
         AbstractMinecartEntity that = (AbstractMinecartEntity) (Object) this;
-        if (that instanceof MinecartEntity) {
+        if (that instanceof MinecartEntity && that.hasPassenger(entity -> entity instanceof PlayerEntity)) {
             cir.setReturnValue(cir.getReturnValueD() * 2);
         }
     }
